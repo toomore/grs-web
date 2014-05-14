db2a9221a7287d9becbf538765a849db3a2d28a1
Toomore Chiang <toomore0929@gmail.com>
2014-05-14 23:47:04 +0800

    Add `ck001` and rewrite.

--- checking.py
+++ checking.py
@@ -1,12 +1,22 @@
 # -*- coding:utf8 -*-
 from grs import Stock
 
+class Checking(object):
 
-class Checking(Stock):
-    def __init__(self, no):
-        super(Stock, self).__init__(no)
+    def __init__(self, data):
+        self.data = data
 
+    def ck001(self):
+        mao = self.data.MAO(3, 6)
+        if mao[0] > 0 and mao[1] > 0 and \
+                self.data.value[-1] > 1000 and self.data.price[-1] > 10 and \
+                (self.data.value[-1] > self.data.value[-2] and self.data.value[-1] > self.data.value[-3]):
+            return True
+
+        return False
 
 if __name__ == "__main__":
-    checking = Checking('2618')
-    print checking.MA(3)
+    checking = Checking(Stock('2618'))
+    print checking.data.MA(3)
+    print checking.ck001()
+    print help(checking)
