45e9ef8a9871417ac62990b6df4a656efa84b51e
Toomore Chiang <toomore0929@gmail.com>
2014-06-10 09:06:13 +0800

    Add realtime.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # -*- coding:utf8 -*-
 from flask import Flask
+from grs import RealtimeTWSE
 from grs import Stock
 from urllib2 import URLError
 
@@ -23,6 +24,11 @@ def stock_page(no):
             return u'No connection'
 
     return u'No stock'
+
+@app.route("/rl/<no>")
+def realtime(no):
+    result = RealtimeTWSE(no)
+    return u'<pre>%s</pre>' % result.data
 
 if __name__ == '__main__':
     app.run(debug=1)
